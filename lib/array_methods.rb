520fb36d1886548bb682d584ec53d5fa8e4701e6
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-22 21:27:22 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,28 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times { |i|
+    if array[i] == value_to_find
+      return i
+    end
+  }
+  return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  max = -1
+  array.length.times { |i|
+    if array[i] > max
+      max = array[i]
+    end
+  }
+  return max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = array.first
+  array.length.times { |i|
+    if array[i] < min
+      min = array[i]
+    end
+  }
+  return min
 end
